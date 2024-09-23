63f330d6b2b9d9afb7d6f744a42392d157d889a3
bruno-yuji <bruno.ytakahashi@sptech.school>
2024-09-23 10:31:59 -0300

    modelagem e script atualizados

--- script_trackSecure.sql
+++ script_trackSecure.sql
@@ -2,25 +2,16 @@ DROP DATABASE IF EXISTS TrackSecure;
 CREATE DATABASE TrackSecure;
  
 USE TrackSecure ;
-
-CREATE TABLE Cargo (
-  idCargo INT PRIMARY KEY AUTO_INCREMENT,
-  nomeCargo VARCHAR(70) NOT NULL,
-  descricao VARCHAR(50) NOT NULL,
-  permissao VARCHAR(45) NOT NULL
-);
-
  
 CREATE TABLE Empresa (
-  idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+  idEmpresa INT PRIMARY KEY,
   cnpj CHAR(14) NOT NULL,
-  nome VARCHAR(60) NOT NULL,
+  razaoSocial VARCHAR(60) NOT NULL,
   cep CHAR(8) NOT NULL,
   logradouro VARCHAR(150) NOT NULL,
-  numero INT NOT NULL,
+  numero VARCHAR(10) NOT NULL,
   telefone CHAR(11) NOT NULL
 );
-
  
 CREATE TABLE Funcionario (
   idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
@@ -28,50 +19,34 @@ CREATE TABLE Funcionario (
   senha VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   cpf CHAR(11) NOT NULL,
-  fkCargo INT NOT NULL,
+  cargo VARCHAR(30) NOT NULL,
   fkEmpresa INT NOT NULL,
-  CONSTRAINT fkUsuarioCargo FOREIGN KEY (fkCargo) REFERENCES Cargo (idCargo),
   CONSTRAINT fkUsuarioEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
-
 
 CREATE TABLE Servidor (
   idServidor INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(80) NULL,
+  sistOperacional VARCHAR(60) NULL,
+  memoriaTotal DECIMAL(5,2) NULL,
+  discoTotal DECIMAL(5,2) NULL,
+  freqMaxProcessador DECIMAL(5,2) NULL,
   fkEmpresa INT NOT NULL,
   CONSTRAINT fkServidorEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
-
  
 CREATE TABLE Registro (
   idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-  so VARCHAR(60) NULL,
   dtHora DATETIME NULL,
   porcentagemProcessador DECIMAL(5,2) NULL,
   porcentagemMemoria DECIMAL(5,2) NULL,
   porcentagemDisco DECIMAL(5,2) NULL,
   freqProcessador DOUBLE NULL,
-  freqMaxProcessador DOUBLE NULL,
-  memoriaTotal DOUBLE NULL,
   memoriaUsada DOUBLE NULL,
-  discoTotal DOUBLE NULL,
   discoUsado DOUBLE NULL,
   fkServidor INT NOT NULL,
   CONSTRAINT fkRegistroServidor FOREIGN KEY (fkServidor) REFERENCES Servidor (idServidor)
 );
-
-
-CREATE TABLE Alerta (
-  idAlerta INT NOT NULL AUTO_INCREMENT,
-  dataHora DATETIME NULL,
-  tipo VARCHAR(45) NULL,
-  descricao VARCHAR(100) NULL,
-  fkRegistro INT NOT NULL,
-  Registro_idRegistro INT NOT NULL,
-  PRIMARY KEY (idAlerta, fkRegistro),
-  CONSTRAINT fkAlertaRegistro FOREIGN KEY (fkRegistro) REFERENCES Registro (idRegistro)
-);
-
 
 CREATE TABLE Estacao (
   idEstacao INT PRIMARY KEY AUTO_INCREMENT,
@@ -82,7 +57,6 @@ CREATE TABLE Estacao (
   CONSTRAINT fkEstacaoEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
-
 CREATE TABLE Catraca (
   idCatraca INT NOT NULL,
   funcionalidade VARCHAR(45) NULL,
@@ -92,8 +66,9 @@ CREATE TABLE Catraca (
   CONSTRAINT fkCatracaServidor FOREIGN KEY (fkServidor) REFERENCES Servidor (idServidor)
 );
 
-INSERT INTO Empresa VALUES (DEFAULT, '18967526172891', 'Metrô de São Paulo', '09987451', 'Rua Leonardo Picasso', 12,'11951296890');
-
-INSERT INTO Servidor VALUES (DEFAULT, 'Servidor 1', 1); 
-
-INSERT INTO Cargo VALUES (DEFAULT, 'Gerente de monitoramento de servidores', 'Gerente do Projeto', 'Total');
+-- CREATE TABLE Alerta (
+--  idAlerta INT PRIMARY KEY AUTO_INCREMENT,
+--  dataHora DATETIME NULL,
+--  tipo VARCHAR(45) NULL,
+--  descricao VARCHAR(100) NULL
+-- );
